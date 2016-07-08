968a0182416ab716b1e596db2d30ae7a62e78ea9
Park Ju Hyung <qkrwngud825@gmail.com>
2016-07-09 00:07:24 +0900

    SystemUI: implement burn-in protection for noti items
    Devices with OLED display suffer from
    status-bar's notification items causing permanent burn-ins when used long-term.
    Most notable example is the battery icon.
    
    Samsung has a workaround for this since the Galaxy S5:
    moving all items in the notification area both horizontally and vertically
    every 30 seconds(or less).
    
    This commit mimics the Samsung's behavior by reading
    "ro.systemui.burn_in_protection" prop.
    (Device maintainers should enable this explicitly.
     Making it togglable from Settings would be also nice.
     Contributions are welcome.)
    
    The "horizontal_max_swift" and "vertical_max_swift" dimens are direct replica
    from Samsung Touchwiz too, and it's most likely the max threshold before it
    annoys the users.
    
    Samsung Touchwiz : https://www.youtube.com/watch?v=78a-bnHdAE0
    This commit : https://twitter.com/arter97/status/749298438508941316
    
    This is obviously not a full solution for burn-ins.
     - Users can still notice the "border line" between status-bar and application
       navigation area when used long-term.
       Fixing this will be much more hacky and it's more of a hardware limitation.
     - Navigation keys(softkeys) also cause burn-ins.
       Moving the navbar buttons with similar concept will also help immensely,
       but since it's not handled from the same source, it's a TODO for another day.
    
    Change-Id: Icfb87b91aa3e314e0040c05152541c5c73a88723
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5105,6 +5105,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mWakeUpTouchLocation = null;
         mStackScroller.setAnimationsEnabled(false);
         updateVisibleToUser();
+        if (mStatusBarView != null && mStatusBarView.shouldEnableSwift())
+            mStatusBarView.clearSwiftHandlerCallbacks();
         if (mLaunchCameraOnFinishedGoingToSleep) {
             mLaunchCameraOnFinishedGoingToSleep = false;
 
@@ -5124,6 +5126,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStackScroller.setAnimationsEnabled(true);
         mNotificationPanel.setTouchDisabled(false);
         updateVisibleToUser();
+        if (mStatusBarView != null && mStatusBarView.shouldEnableSwift())
+            mStatusBarView.initSwiftHandlerCallbacks();
     }
 
     public void onScreenTurningOn() {
