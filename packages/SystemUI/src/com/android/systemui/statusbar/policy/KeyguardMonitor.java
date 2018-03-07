3807b27bb542ad0834364199ffd22db5700d490c
klozz <klozz707@gmail.com>
2018-03-07 06:49:29 -0500

    Revert "SystemUI: KeyGuardMonitor: Prevent ConcurrentModificationException"
    This reverts commit d91c7caf237e83d3be1eca4267144fb9560badf8.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyguardMonitor.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyguardMonitor.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
- * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -128,7 +127,8 @@ public final class KeyguardMonitor extends KeyguardUpdateMonitorCallback {
     }
 
     private void notifyKeyguardChanged() {
-        for (Callback callback : callbacks) {
+        for (int i = 0; i < mCallbacks.size(); i++) {
+            Callback callback = mCallbacks.get(i);
             callback.onKeyguardChanged();
         }
     }
